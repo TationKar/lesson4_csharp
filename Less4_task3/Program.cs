800fb2f221a90da6087d487b2856ddc4e3d6d003
Timur Karimov <tationkar@gmail.com>
2023-06-04 15:27:59 +0300

    all

---
+++ Less4_task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Factorial
+Console.Clear();
+
+double Factorial(double n)
+{
+    if (n==1) return 1;
+    else {
+        return n * Factorial(n-1);
+    }
+
+}
+
+
+
+Console.Write("Введите число для нахождения его факториала: ");
+double factr = double.Parse(Console.ReadLine());
+for (double i = 1; i <= factr; i++)
+{
+    Console.WriteLine($"Factorial {i}! = " + Factorial(i));
+} 
+Console.WriteLine(Factorial(factr));
